bf6d4a21d25200d5710531da4dc79d58ae93b9ca
Defective <def3ctive4@gmail.com>
2026-01-27 19:21:13 +0100

    Remove the method for creating temporary directories

--- src/main/java/io/github/defective4/tv/dvbservices/util/TemporaryFiles.java
+++ src/main/java/io/github/defective4/tv/dvbservices/util/TemporaryFiles.java
@@ -7,22 +7,9 @@ import java.nio.file.Files;
 public class TemporaryFiles {
     private TemporaryFiles() {}
 
-    public static File getTemporaryDir() throws IOException {
-        File dir = Files.createTempDirectory("dvbsvc").toFile();
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> remove(dir)));
-        return dir;
-    }
-
     public static File getTemporaryFile(String suffix) throws IOException {
         File file = Files.createTempFile("dvbsvc", "." + suffix).toFile();
         file.deleteOnExit();
         return file;
-    }
-
-    private static void remove(File dir) {
-        if (dir.isDirectory()) {
-            for (File f : dir.listFiles()) remove(f);
-        }
-        dir.delete();
     }
 }
